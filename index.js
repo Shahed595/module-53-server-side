96dbd30c28b1fb5a7b520799eb8422ebc5d8961f
Shahed595 <multitaskershahed1@hmail.com>
2021-12-20 20:26:48 +0600

    create-dynamic api-api parameter,access params

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ app.get('/',(req,res)=>{
 })
 
 
+const users=[
+    {id:0,name:"Shabana",email:"shabana@gamil.com",phone:"01893428883"},
+    {id:1,name:"rabeya",email:"rabeya@gamil.com",phone:"01893428883"},
+    {id:2,name:"shucorita",email:"shucorita@gamil.com",phone:"01893428883"},
+    {id:3,name:"susmita",email:"susmita@gamil.com",phone:"01893428883"},
+    {id:4,name:"sagorika",email:"sagorika@gamil.com",phone:"01893428883"},
+    {id:5,name:"kobori",email:"kobori@gamil.com",phone:"01893428883"},
+]
+
+app.get('/users',(req,res)=>{
+    res.send(users);
+})
+//dynamic,api,params
+app.get('/users/:id',(req,res)=>{
+    const id=req.params.id;
+    const user=users[id];
+    res.send(user);
+    // console.log(req.params.id);
+})
+
 app.listen(port,()=>{
     console.log("listening port",port);
 })
